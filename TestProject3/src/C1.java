0c84905cdf69aaa9d83f217935baabb97223b95c
bvelez <Bienvenido.Velez@upr.edu>
2013-10-17 22:54:25 -0400

    Initial commit

---
+++ TestProject3/src/C1.java
@@ -0,0 +1,8 @@
+
+public class C1 {
+
+	public C1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TestProject3/src/C2.java
@@ -0,0 +1,8 @@
+
+public class C2 {
+
+	public C2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
